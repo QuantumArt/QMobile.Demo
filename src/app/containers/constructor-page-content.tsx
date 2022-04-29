7c50069b5acbd5a326e9b8d5ec6997717e4de123
ARTQ\ValyaevE <ValyaevE@quantumart.ru>
2022-04-29 17:39:10 +0300

    init branch

--- src/app/containers/constructor-page-content.tsx
+++ src/app/containers/constructor-page-content.tsx
@@ -4,6 +4,7 @@ const ConsructorPageContent: FC = () => {
   return (
     <div className="constructor-page-content">
       <p className="page-content-title page-content-title--constructor">Соберите свой тариф</p>
+      
     </div>
   );
 };
