51c72ce8dd183239db0bc08f1b33a32fc7a04c14
ARTQ\ValyaevE <ValyaevE@quantumart.ru>
2022-05-26 16:26:49 +0300

    parse description as html

--- src/app/containers/device-details/tabs/device-description-tab.tsx
+++ src/app/containers/device-details/tabs/device-description-tab.tsx
@@ -1,12 +1,14 @@
 import { useObserver } from 'mobx-react-lite';
 import React from 'react';
+import parse from 'html-react-parser';
+
 import devicesStore from '../../../../stores/devices/devices-store';
 
 const DeviceDescriptionTab = (): JSX.Element => {
   return useObserver(() => (
     <div>
       <h3 className="device-details__description-tab-title">Описание</h3>
-      <p>{devicesStore.description}</p>
+      <p>{parse(devicesStore.description ?? 'Нет описания')}</p>
     </div>
   ));
 };
