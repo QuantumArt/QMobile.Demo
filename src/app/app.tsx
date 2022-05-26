e44776f6e22895a936549fe596132513322a90f2
ARTQ\ValyaevE <ValyaevE@quantumart.ru>
2022-05-26 11:51:45 +0300

    fix styles

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -62,7 +62,7 @@ function App(): JSX.Element {
   return (
     <Router>
       <Header />
-      <main className="main">
+      <main>
         <Routes>
           {routes.map(({ path, element }) => (
             <Route key={path} path={path} element={element} />
