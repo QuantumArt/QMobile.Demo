8cddfcb4698e64925564d43a54100c0c5683cdb9
Pavel Celut <CelutP@gmail.com>
2024-02-18 20:12:59 +0300

    Use .env

---
+++ install/compose/demo.js
@@ -0,0 +1,7 @@
+window.env = {
+  DPC_HOST: 'http://localhost:7500',
+  IMPACT_HOST: 'http://localhost:8100',
+  DOWNLOAD_PDF_HOST: 'http://localhost:7900'
+};
+
+
---
+++ install/compose/demohost.js
@@ -0,0 +1,7 @@
+window.env = {
+  DPC_HOST: 'http://dpc-search-api.test',
+  IMPACT_HOST: 'http://dpc-impact-api.test',
+  DOWNLOAD_PDF_HOST: 'http://dpc-pdf-server.test'
+};
+
+
